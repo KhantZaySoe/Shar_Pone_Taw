59cabfb70bef10124869a7e9e415121db66bd52f
khantzaesoe <khantzaysoe002@gmail.com>
2026-06-14 02:00:26 +0630

    complete front end

---
+++ js/booking_doctor_db.js
@@ -0,0 +1,138 @@
+const doctors_db = [
+  // --- အကယ်ဒမီဆေးရုံ (hospital_id: 1) ---
+  {
+    d_id: 101,
+    name: "Dr. အောင်မင်း",
+    category: "General Physician (အထွေထွေရောဂါကု)",
+    hospital_id: 1, 
+    experience: "10 Years",
+    rating: 4.8,
+    image:"../images/do2.png"
+  },
+  {
+    d_id: 102,
+    name: "Dr. စုမြတ်",
+    category: "Cardiology (နှလုံး)",
+    hospital_id: 1,
+    experience: "15 Years",
+    rating: 4.9,
+    image:"../images/d2.png"
+  },
+  {
+    d_id: 103,
+    name: "Dr. ဇော်မင်း",
+    category: "Pediatrics (ကလေး)",
+    hospital_id: 1,
+    experience: "8 Years",
+    rating: 4.7,
+    image:"../images/do5.png"
+  },
+
+  // --- ပါရမီအထွေထွေရောဂါကုဆေးရုံ (hospital_id: 2) ---
+  {
+    d_id: 104,
+    name: "Dr. သဇင်",
+    category: "Obstetrics & Gynecology (သားဖွားမီးယပ်)",
+    hospital_id: 2,
+    experience: "12 Years",
+    rating: 4.8,
+  },
+  {
+    d_id: 105,
+    name: "Dr. ကျော်ဇင်",
+    category: "Neurology (ဦးနှောက်နှင့်အာရုံကြော)",
+    hospital_id: 2,
+    experience: "20 Years",
+    rating: 5.0,
+  },
+
+  // --- ပင်လုံဆေးရုံ (hospital_id: 3) ---
+  {
+    d_id: 106,
+    name: "Dr. ဇော်ရဲ",
+    category: "Orthopedics (အရိုးအကြော)",
+    hospital_id: 3,
+    experience: "12 Years",
+    rating: 4.7,
+  },
+  {
+    d_id: 107,
+    name: "Dr. နန်းဆု",
+    category: "General Physician (အထွေထွေရောဂါကု)",
+    hospital_id: 3,
+    experience: "5 Years",
+    rating: 4.5,
+  },
+
+  // --- အာယုအပြည်ပြည်ဆိုင်ရာဆေးရုံ (hospital_id: 5) ---
+  {
+    d_id: 108,
+    name: "Dr. နီလာ",
+    category: "Cardiology (နှလုံး)",
+    hospital_id: 5,
+    experience: "15 Years",
+    rating: 4.9,
+  },
+  {
+    d_id: 109,
+    name: "Dr. ဝဏ္ဏ",
+    category: "ENT (နား၊ နှာခေါင်း၊ လည်ချောင်း)",
+    hospital_id: 5,
+    experience: "11 Years",
+    rating: 4.6,
+  },
+
+  // --- ကန်သာယာ အပြည်ပြည်ဆိုင်ရာ အထူးကုဆေးရုံ (hospital_id: 10) ---
+  {
+    d_id: 110,
+    name: "Dr. စုစုလှိုင်",
+    category: "Dermatology (အရေပြား)",
+    hospital_id: 10,
+    experience: "9 Years",
+    rating: 4.7,
+  },
+  {
+    d_id: 111,
+    name: "Dr. ဝေယံ",
+    category: "Ophthalmology (မျက်စိ)",
+    hospital_id: 10,
+    experience: "11 Years",
+    rating: 4.6,
+  },
+
+  // --- မန္တလေးပြည်သူ့ဆေးရုံကြီး (hospital_id: 17) ---
+  {
+    d_id: 112,
+    name: "Dr. မြတ်နိုး",
+    category: "General Physician (အထွေထွေရောဂါကု)",
+    hospital_id: 17,
+    experience: "5 Years",
+    rating: 4.5,
+  },
+  {
+    d_id: 113,
+    name: "Dr. ရန်နိုင်",
+    category: "General Surgery (ခွဲစိတ်)",
+    hospital_id: 17,
+    experience: "18 Years",
+    rating: 4.9,
+  },
+  {
+    d_id: 114,
+    name: "Dr. အေးအေး",
+    category: "Pediatrics (ကလေး)",
+    hospital_id: 17,
+    experience: "14 Years",
+    rating: 4.8,
+  },
+  
+  // --- ရွှေလမင်းဆေးရုံ (hospital_id: 9) ---
+  {
+    d_id: 115,
+    name: "Dr. ထွန်းထွန်း",
+    category: "Gastroenterology (အစာအိမ်နှင့်အူလမ်းကြောင်း)",
+    hospital_id: 9,
+    experience: "14 Years",
+    rating: 4.8,
+  }
+];
---
+++ js/hospital_db.js
@@ -0,0 +1,121 @@
+const hospital_db = [
+  {
+    h_id: 1,
+    name: "အကယ်ဒမီဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h1.jpg"
+  },
+  {
+    h_id: 2,
+    name: "ပါရမီအထွေထွေရောဂါကုဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h2.jpg"
+  },
+  {
+    h_id: 3,
+    name: "ပင်လုံဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h3.jpg"
+  },
+  {
+    h_id: 4,
+    name: "အောင်ရတနာဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h4.jpg"
+  },
+  {
+    h_id: 5,
+    name: "အာယုအပြည်ပြည်ဆိုင်ရာဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h5.jpg"
+  },
+  {
+    h_id: 6,
+    name: "SSC ရွှေဂုံတိုင်အထူးကုဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h6.jpg"
+  },
+  {
+    h_id: 7,
+    name: "KBC ဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h7.jpg"
+  },
+  {
+    h_id: 8,
+    name: "ဥက္ကလာဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h8.jpg"
+  },
+  {
+    h_id: 9,
+    name: "ရွှေလမင်းဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h9.jpg"
+  },
+  {
+    h_id: 10,
+    name: "ကန်သာယာ အပြည်ပြည်ဆိုင်ရာ အထူးကုဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h10.jpg"
+  },
+  {
+    h_id: 11,
+    name: "ရိုစ်ဟေးလ်ဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h11.jpg"
+  },
+  {
+    h_id: 12,
+    name: "တက်လမ်းဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h12.jpg"
+  },
+  {
+    h_id: 13,
+    name: "မြပါရမီဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h13.jpg"
+  },
+  {
+    h_id: 14,
+    name: "ဖမ်မလီ အထွေထွေရောဂါကုဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h14.jpg"
+  },
+  {
+    h_id: 15,
+    name: "ဆာကူရာဆေးရုံ",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h15.jpg"
+  },
+  {
+    h_id: 16,
+    name: "The Clinic De Novo ဆေးခန်း",
+    location: "ရန်ကုန်မြို့",
+    phone: "09-000000000",
+    image: "../images/h16.jpg"
+  },
+  {
+    h_id: 17,
+    name: "မန္တလေးပြည်သူ့ဆေးရုံကြီး",
+    location: "မန္တလေးမြို့",
+    phone: "09-000000000",
+    image: "../images/h17.jpg"
+  }
+];
